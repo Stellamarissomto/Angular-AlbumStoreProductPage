c39f57b3140de3df9a7376fbcdfa5e43681dd993
Stellamarissomto <stellamarissomto@gmail.com>
2019-10-30 20:13:48 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,7 +21,8 @@ export class ProductService {
 
  }
      getProducts() {
-      return this._http.get(this._productsUrl)
+      return this._http.get(this._productsUrl).map((response) => 
+      response.json());
 }
 
 }
