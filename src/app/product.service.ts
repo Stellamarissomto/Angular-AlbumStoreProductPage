457c58c932ee68decdcb894521b1848a76aa1be2
Stellamarissomto <stellamarissomto@gmail.com>
2019-10-30 20:21:41 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -20,9 +20,9 @@ export class ProductService {
   <Album>response.json());
 
  }
-     getProducts() {
+     getProducts(): Observable<Product[]> {
       return this._http.get(this._productsUrl).map((response) => 
-      response.json());
+      <Product[]>response.json());
 }
 
 }
